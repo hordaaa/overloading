3e3fa07b3288534976e8359153e6e856ce6df42c
Bakulin Denis <dtorkon@yandex.ru>
2024-06-22 22:52:41 +0700

    refactor

--- overload.py
+++ overload.py
@@ -1,5 +1,5 @@
 from types import UnionType, FunctionType
-from typing import get_args
+from typing import get_args, NoReturn
 
 
 class NoRegisterArgumentError(Exception):
@@ -13,7 +13,7 @@ class OverloadDecorator:
     При этом перегружать функцию можно несколькими типами (передав в register несколько аргументов).
     """
 
-    def __init__(self, default: FunctionType):
+    def __init__(self, default: FunctionType) -> None:
         """
         Дефолтная функция будет вызвана, если нет совпадений у зарегистрированных
         ранее функций или в нее не было передано аргументов
@@ -22,8 +22,8 @@ class OverloadDecorator:
         self.default = default
         self.validators = {}
 
-    def __register_annotation(self, function: FunctionType):
-        """Регистрирует аннотацию типа первого параметра фкнкции"""
+    def __register_with_annotation(self, function: FunctionType) -> FunctionType:
+        """Регистрирует реализацию функции по аннотации типа первого параметра фкнкции"""
 
         if function.__annotations__.values():
             if isinstance(datatype := list(function.__annotations__.values())[0], UnionType):
@@ -32,36 +32,37 @@ class OverloadDecorator:
                 self.validators[(datatype,)] = function
         return function
 
-    def register(self, *datatype: FunctionType | type | tuple[type, ...]):
-        """Регистрирует тип/ы данных для перегрузки функции"""
+    def __register_with_types(self, function: FunctionType, datatypes: tuple[type, ...]) -> FunctionType:
+        """Регистрирует реализацию функции по переданным в register типам"""
 
-        if not datatype:
+        self.validators[datatypes] = function
+        return function
+
+    def register(self, *datatypes: FunctionType | type | tuple[type, ...]) -> NoReturn | FunctionType:
+        """Регистрирует реализацию функции по переданным аргументам"""
+
+        if not datatypes:
             raise NoRegisterArgumentError
 
-        if isinstance(datatype[0], FunctionType):
-            return self.__register_annotation(datatype[0])
+        if isinstance(datatypes[0], FunctionType):
+            return self.__register_with_annotation(datatypes[0])
 
-        return lambda func: self(func, _is_main_call=False, _datatype=datatype)
+        return lambda func: self.__register_with_types(func, datatypes=datatypes)
 
-    def __call__(self, *args, _is_main_call=True, _datatype=(), **kwargs):
+    def __call__(self, *args, _datatype=(), **kwargs) -> FunctionType:
 
-        if _is_main_call:
+        if not args or type(args[0]) not in sum(tuple(self.validators), start=()):
+            return self.default(*args, **kwargs)
 
-            if not args or type(args[0]) not in sum(tuple(self.validators), start=()):
-                return self.default(*args, **kwargs)
-
-            for validators in tuple(self.validators)[::-1]:
-                if type(args[0]) in validators:
-                    return self.validators[validators](*args, **kwargs)
-
-        self.validators[_datatype] = args[0]
-        return args[0]
+        for validators in tuple(self.validators)[::-1]:
+            if type(args[0]) in validators:
+                return self.validators[validators](*args, **kwargs)
 
 
-def overload(func: FunctionType) -> OverloadDecorator:
+def overload(function: FunctionType) -> OverloadDecorator:
     """Декорирует функцию для ее перегрузки"""
 
-    return OverloadDecorator(func)
+    return OverloadDecorator(function)
 
 
 if __name__ == "__main__":
